120741a53f72a2cda1dcd106f338b3860a0004b0
A.Gökhan SATMAN <abgsatman@hotmail.com>
2021-08-18 20:53:52 +0300

    character controller

---
+++ 2D Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Vector3 move;
+
+    [SerializeField]
+    private float speed = 5f;
+
+    [SerializeField]
+    private float jumpSpeed = 3f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = new Vector3(Input.GetAxis("Horizontal"), 0f);
+        transform.position += move * speed * Time.deltaTime;
+
+        if(Input.GetButtonDown("Jump") && Mathf.Approximately(rb.velocity.y, 0f))
+        {
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode2D.Impulse);
+        }
+
+        if(Input.GetAxisRaw("Horizontal") == 1)
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        } 
+        else if(Input.GetAxisRaw("Horizontal") == -1)
+        {
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+
+    }
+}
