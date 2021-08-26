bd58b27a03de471587b686df9cb321aec965cfe3
A.Gökhan SATMAN <abgsatman@hotmail.com>
2021-08-26 20:42:41 +0300

    coin animation

---
+++ 2D Platformer/Assets/Scripts/CoinController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    public Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        StartCoroutine(RandomizeCoin());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator RandomizeCoin()
+    {
+        float randomTimeSecond = Random.Range(0.01f, 0.5f);
+        yield return new WaitForSeconds(randomTimeSecond);
+        animator.enabled = true;
+    }
+}
